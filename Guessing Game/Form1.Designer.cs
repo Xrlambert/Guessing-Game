3f86dbde00a0f2e6ac76e464955c22a3363cdcd5
Xavier <Xavier@SILENTSTORM>
2025-10-05 17:08:42 -0400

    Add project files.

---
+++ Guessing Game/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Guessing_Game
+{
+    partial class Guessing_Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Guessing_Game));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.GuessBox = new System.Windows.Forms.TextBox();
+            this.GuessButton = new System.Windows.Forms.Button();
+            this.feedback = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.testDisp = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(65, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guessing Game";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(59, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter a Number Between 1 and 100";
+            // 
+            // GuessBox
+            // 
+            this.GuessBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessBox.Location = new System.Drawing.Point(176, 175);
+            this.GuessBox.Name = "GuessBox";
+            this.GuessBox.Size = new System.Drawing.Size(38, 29);
+            this.GuessBox.TabIndex = 2;
+            this.GuessBox.TextChanged += new System.EventHandler(this.GuessBox_TextChanged);
+            // 
+            // GuessButton
+            // 
+            this.GuessButton.BackColor = System.Drawing.Color.Black;
+            this.GuessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessButton.ForeColor = System.Drawing.Color.White;
+            this.GuessButton.Location = new System.Drawing.Point(152, 229);
+            this.GuessButton.Name = "GuessButton";
+            this.GuessButton.Size = new System.Drawing.Size(84, 45);
+            this.GuessButton.TabIndex = 3;
+            this.GuessButton.Text = "Guess";
+            this.GuessButton.UseVisualStyleBackColor = false;
+            this.GuessButton.Click += new System.EventHandler(this.GuessButton_Click);
+            // 
+            // feedback
+            // 
+            this.feedback.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.feedback.ForeColor = System.Drawing.Color.White;
+            this.feedback.Location = new System.Drawing.Point(12, 304);
+            this.feedback.Name = "feedback";
+            this.feedback.Size = new System.Drawing.Size(364, 20);
+            this.feedback.TabIndex = 4;
+            this.feedback.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.feedback.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(63, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Please Enter A Number";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Visible = false;
+            // 
+            // testDisp
+            // 
+            this.testDisp.AutoSize = true;
+            this.testDisp.ForeColor = System.Drawing.Color.Cornsilk;
+            this.testDisp.Location = new System.Drawing.Point(16, 22);
+            this.testDisp.Name = "testDisp";
+            this.testDisp.Size = new System.Drawing.Size(16, 13);
+            this.testDisp.TabIndex = 6;
+            this.testDisp.Text = "---";
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Black;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.White;
+            this.resetButton.Location = new System.Drawing.Point(289, 175);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(66, 29);
+            this.resetButton.TabIndex = 7;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Visible = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 8;
+            // 
+            // Guessing_Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(388, 360);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.testDisp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.feedback);
+            this.Controls.Add(this.GuessButton);
+            this.Controls.Add(this.GuessBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Guessing_Game";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox GuessBox;
+        private System.Windows.Forms.Button GuessButton;
+        private System.Windows.Forms.Label feedback;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label testDisp;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Guessing Game/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guessing_Game
+{
+    public partial class Guessing_Game : Form
+    {
+        Random randGen = new Random();
+        int userInput;
+        int secretNumber;
+        int guessCount = 1;
+        public Guessing_Game()
+        {
+            InitializeComponent();
+            secretNumber = randGen.Next(1, 101);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void GuessButton_Click(object sender, EventArgs e)
+        {
+            
+            testDisp.Text = secretNumber.ToString();
+            SoundPlayer error = new SoundPlayer(Properties.Resources.errorSfx);
+            SoundPlayer ding = new SoundPlayer(Properties.Resources.dingSfx);
+            SoundPlayer nope = new SoundPlayer(Properties.Resources.nopeSfx);
+            // Parse user input from text box
+
+
+            if (int.TryParse(GuessBox.Text, out userInput) && userInput >= 1 && userInput <= 100)
+            {
+                //create difference between the two numbers for hot cold comparison
+                int diff = (secretNumber - userInput);
+
+                guessCount++;
+
+                //call function for response
+                if (diff < 0)
+                {
+                    feedback.Text = GetHint(diff) + ", Too High!";
+                    nope.Play();
+
+                }
+                else if (diff > 0)
+                {
+                    feedback.Text = GetHint(diff) + ", Too Low";
+                    nope.Play();
+
+                }
+                else
+                {
+                    feedback.Text = GetHint(diff);
+                    ding.Play();
+                    resetButton.Text = GetHint(diff);
+                }
+
+
+            }
+            else
+            {
+                feedback.ForeColor = Color.Red;
+                feedback.Text = "Invalid Input";
+                error.Play();
+                await Task.Delay(1200);
+                feedback.Text = "";
+                feedback.ForeColor = Color.White;
+                return;
+            }
+            
+            if (userInput == secretNumber)
+            {
+                resetButton.Visible = true;
+            }
+
+
+        }
+
+        private void GuessBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //function for finding the most appropriate response for user input compared with the number
+        private string GetHint(int difference)
+        {
+            difference = Math.Abs(difference);
+            if (difference >= 50) return "Freezing";
+            if (difference >= 25) return "Cold";
+            if (difference >= 15) return "Cool";
+            if (difference >= 10) return "Warm";
+            if (difference >= 5) return "Hot";
+            if (difference >= 1) return "Boiling";
+            return "Correct! You guessed the number!";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            resetButton.Visible = false;
+            secretNumber = randGen.Next(1, 101);
+        }
+    }
+}
